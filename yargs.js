37ec8c620cb3f54915b9b76adab8a5f68adc4091
pavan079 <144233598+pavan079@users.noreply.github.com>
2023-09-15 10:26:08 +0530

    Add files via upload

---
+++ yargs.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+
+const args = require('yargs').argv;
+
+fs.readFile('audit.txt', 'utf8', (err,data) =>{
+    if(err){
+        console.log(err)
+    }
+
+    else{
+        console.log(data,'able to read the data successfully');
+        const fileArray = data.split('/n')
+        if(fileArray.indexOf(args.filename) === -1){
+            fs.writeFile('')
+        }
+
+    }
+})
